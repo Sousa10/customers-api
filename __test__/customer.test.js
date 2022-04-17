30ed540c543e6406e06941f1cffab15eb432a709
Rui Sousa <ruisousa05@gmail.com>
2022-04-16 23:24:34 -0100

    added tests

---
+++ __test__/customer.test.js
@@ -0,0 +1,134 @@
+const customers = require('../routes/customer')
+const request = require('supertest')
+const express = require('express')
+let customersData = require('../data')
+
+const app = express()
+
+
+// parse json
+app.use(express.json())
+
+app.use('/api/customer', customers)
+
+describe('Customers API', () => {
+    test('GET /customers --> all customers', () => {
+        return request(app)
+            .get('/api/customer/')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .then((response) => {
+                expect(response.body).toEqual(
+                    expect.objectContaining({
+                        success: true,
+                        data: expect.arrayContaining([
+                            expect.objectContaining({
+                                id: expect.any(Number),
+                                name: expect.any(String),
+                                surname: expect.any(String),
+                                email: expect.any(String),
+                                birthdate: expect.any(String),
+                            })
+                        ])
+                    })
+                )
+            })
+    })
+
+    it('GET /customers/id --> specific customer by ID', () => {
+        return request(app)
+            .get('/api/customer/1')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .then((response) => {
+                expect(response.body).toEqual(
+                    expect.objectContaining({
+                        success: true,
+                        data: expect.objectContaining({
+                            id: expect.any(Number),
+                            name: expect.any(String),
+                            surname: expect.any(String),
+                            email: expect.any(String),
+                            birthdate: expect.any(String),
+                        })
+                    })
+                )
+            })
+    })
+
+    it('GET /customers/id --> 404 if not found', () => {
+        return request(app)
+            .get('/api/customer/9999')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(404)
+            .then((response) => {
+                expect(response.body).toEqual(
+                    expect.objectContaining({
+                        success: false,
+                        msg: 'no customer with id 9999'
+                    })
+                )
+            })
+    })
+
+    it('POST /customers --> create customer', () => {
+        const data = { id: customersData.length + 1, name: "Lorem", surname: 'Ipsum', email: 'lorem@gmail.com', birthdate: '12/03/1994' };
+        return request(app).post('/api/customer/').send(data)
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(201)
+            .then((response) => {
+                expect(response.body).toEqual(
+                    expect.objectContaining({
+                        success: true,
+                        data: data
+                    })
+                )
+            })
+    })
+
+    it('PUT /customers/id --> update customer', async () => {
+        return request(app)
+            .patch('/api/customer/1')
+            .send({ name: "Lorem2" })
+            .expect(200)
+            .then((response) => {
+                expect(response.body).toEqual(
+                    expect.objectContaining({
+                        success: true,
+                        data: expect.arrayContaining([
+                            expect.objectContaining({
+                                id: expect.any(Number),
+                                name: expect.any(String),
+                                surname: expect.any(String),
+                                email: expect.any(String),
+                                birthdate: expect.any(String),
+                            })
+                        ])
+                    })
+                )
+            })
+    })
+
+    it('DELETE /customers/id --> delete customer', () => {
+        return request(app)
+            .delete('/api/customer/1')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .then((response) => {
+                expect(response.body).toEqual(
+                    expect.objectContaining({
+                        success: true,
+                        data: expect.arrayContaining([
+                            expect.objectContaining({
+                                id: expect.any(Number),
+                                name: expect.any(String),
+                                surname: expect.any(String),
+                                email: expect.any(String),
+                                birthdate: expect.any(String),
+                            })
+                        ])
+                    })
+                )
+            })
+    })
+})
--- controllers/customers.js
+++ controllers/customers.js
@@ -6,15 +6,27 @@ const getCustomers = ((req, res) => {
 
 const getCustomer = ((req, res) => {
     const singleCustomer = customers.find((customer) => customer.id === Number(req.params.id))
+    if (!singleCustomer) {
+        return res.status(404).json({ success: false, msg: `no customer with id ${req.params.id}`})
+    }
     res.status(200).json({success: true, data: singleCustomer})
 })
 
 const createCustomer = ((req, res) => {
-    const { name, surname, email, birthdate } = req.body
-    if (!name || !surname || !email || !birthdate) {
+    //const { name, surname, email, birthdate } = req.body
+    const { body } = req
+    if (!body.name || !body.surname || !body.email || !body.birthdate) {
         return res.status(400).json({ success: false, msg: 'please provide all the required information' })
     }
-    res.status(201).send({success: true, data: [...customers, {name: name, surname: surname, email: email, birthdate: birthdate}]})
+    const newCustomer = {
+        id: customers.length + 1,
+        name: body.name,
+        surname: body.surname,
+        email: body.email,
+        birthdate: body.birthdate
+    }
+    customers.push(newCustomer)
+    res.status(201).json({success: true, data: newCustomer})
 })
 
 const updateCustomer = ((req, res) => {
